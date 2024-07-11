4c2bccb43df0337caad45f59f4e4ecaecf9f811a
programmer-rahul <7678478061k@gmail.com>
2024-07-11 11:49:00 +0530

    SET GET SETNX MGET

--- index.ts
+++ index.ts
@@ -4,6 +4,7 @@ const redisClient = redis.createClient();
 
 redisClient.on("connect", () => {
   console.log("Connected to Redis");
+  // redisClient.set('redisKey',"working redis")
 });
 
 redisClient.on("error", () => {
@@ -11,3 +12,24 @@ redisClient.on("error", () => {
 });
 
 redisClient.connect();
+
+const init = async () => {
+  // SET
+  const redisSET = await redisClient.SET("username", "redis123");
+  console.log("redisSET", redisSET);
+
+  // GET
+  const redisGET = await redisClient.GET("username");
+  console.log("redisGET", redisGET);
+
+  // SETNX
+  // for unique keys
+  const redisSETNX = await redisClient.SETNX("username", "new username");
+  console.log("redisSETNX", redisSETNX);
+
+  // MGET
+  const redisMGET = await redisClient.MGET(["username","new username"]);
+  console.log('redisMGET',redisMGET);
+};
+
+init();
